6a2feac1d8a3214c3216cc7480e8c4fcbf91f394
Matthew Tribby <mrt28@duke.edu>
2018-05-31 00:00:27 -0400

    Added full screen and historic pmaps

--- js/pmaps.js
+++ js/pmaps.js
@@ -1,11 +1,20 @@
 
-  var map;
 
-  function initMap() {
-    return(
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 35.285472, lng: -82.790922},
-        zoom: 11
-      })
-    );
-  }
+//global variable map. Still trying to write in functional style though
+var map;
+//Used for testing markers
+var fishHatch = {lat: 35.285472, lng: -82.790922};
+
+//Adding to global scope because needs to be accessed fast due to async load of Google Maps api
+//Reference: https://stackoverflow.com/questions/34466718/googlemaps-does-not-load-on-page-load
+window.initMap = function() {
+    var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 11, center: fishHatch});
+
+    addMarker(map, fishHatch)
+}
+
+function addMarker(map, position){
+  return(new google.maps.Marker(
+    {position: {lat: position.lat, lng: position.lng}, map: map}));
+}
